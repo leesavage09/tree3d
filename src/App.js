18becb33d9845c4c404d5feff125608c94fee896
leesavage09 <leesavage09@gmail.com>
2022-09-26 18:36:34 +0300

    added react-three/fiber

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React, { Component} from "react";
-import {hot} from "react-hot-loader";
+import React, { useRef, useState, Component } from "react";
+import { hot } from "react-hot-loader";
+import { Canvas, useFrame } from '@react-three/fiber'
 import "./App.css";
 
-class App extends Component{
-  render(){
-    return(
+const Box = (props) => {
+  const ref = useRef()
+
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+class App extends Component {
+  render() {
+    return (
       <div className="App">
         <h1> Hello, World!</h1>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
       </div>
     );
   }
