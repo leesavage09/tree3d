71e94dc6e5c66d3c74d218836f11b1ab0c0ab9ad
leesavage09 <leesavage09@gmail.com>
2022-09-27 18:55:07 +0300

    math helpers

---
+++ src/components/util/math.ts
@@ -0,0 +1,12 @@
+import { Vector3 } from "three"
+
+export const findPosition = (yaw: number, pitch: number, distance: number) => {
+    const Px = distance * Math.sin(yaw) * Math.cos(pitch)
+    const Pz = distance * Math.sin(pitch)
+    const Py = distance * Math.cos(yaw) * Math.cos(pitch)
+
+    return new Vector3(Px, Py, Pz)
+}
+
+
+export const DegToRad = (deg: number) => deg * Math.PI / 180
