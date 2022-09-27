7de0b8d5ebc7ca718642eed578eb0004e46e2d33
leesavage09 <leesavage09@gmail.com>
2022-09-27 18:56:50 +0300

    root branches can now be placed by clicking

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { hot } from "react-hot-loader";
-import { Canvas } from '@react-three/fiber'
+import { Canvas, ThreeEvent, useFrame } from '@react-three/fiber'
 import { Ground } from "./components/objects/ground";
 import { CameraController } from "./components/objects/cameraController";
 import { Branch } from "./components/objects/branch";
+import uuid from 'react-uuid';
 import "./App.css";
 
-const DegToRad = (deg: number) => deg * Math.PI / 180
-
 const App: React.FC = () => {
+  const [roots, setRoots] = useState<JSX.Element[]>([])
+
+  const handleGroundClicked = (e: ThreeEvent<MouseEvent>) => {
+    if (e.delta < 10) {
+      setRoots(roots => [...roots, <Branch key={uuid()} start={e.point} pitch={0} yaw={0} dia_start={1} dia_end={0.5} len={3} />])
+    }
+  }
+
   return (
     <div className="App">
-      <h1> Hello, World!</h1>
       <Canvas camera={{ fov: 45, position: [25, 5, 25] }}>
         <CameraController />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Ground />
-        <Branch start={[0, 0, 0]} angle_vir={0} angle_hor={0} dia_start={2} dia_end={0.1} length={10} />
-        <Branch start={[0, 2.5, 0]} angle_vir={DegToRad(45)} angle_hor={DegToRad(0)} dia_start={1} dia_end={0.5} length={3} />
+        <Ground onClick={handleGroundClicked} />
+        {roots}
       </Canvas>
     </div>
   );
