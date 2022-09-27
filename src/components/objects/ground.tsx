e2a4596a1c9975f79adc45d478b49e487f42f19c
leesavage09 <leesavage09@gmail.com>
2022-09-27 18:55:22 +0300

    ground now accepts mesh properties

--- src/components/objects/ground.tsx
+++ src/components/objects/ground.tsx
@@ -1,8 +1,10 @@
+import { MeshProps } from "@react-three/fiber"
 import React from "react"
 
-export const Ground: React.FC = () => {
+export const Ground: React.FC<MeshProps> = (props) => {
     return (
         <mesh
+            {...props}
             rotation={[-1.5708, 0, 0]}
             position={[0, 0, 0]}
         >
