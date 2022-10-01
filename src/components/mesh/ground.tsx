9b9003a58c127f32b1dd550503e6b1915ab2b7cb
leesavage09 <leesavage09@gmail.com>
2022-10-01 16:29:13 +0300

    enlarge ground

--- src/components/mesh/ground.tsx
+++ src/components/mesh/ground.tsx
@@ -8,7 +8,7 @@ export const Ground: React.FC<MeshProps> = (props) => {
             rotation={[-1.5708, 0, 0]}
             position={[0, 0, 0]}
         >
-            <planeGeometry args={[100, 100]} />
+            <planeGeometry args={[10000, 10000]} />
             <meshStandardMaterial color={'green'} />
         </mesh>
     )
