ef0c1f543b4f895b60333de4ff72817e1a9f0761
leesavage09 <leesavage09@gmail.com>
2022-10-01 16:29:24 +0300

    side angle helper

--- src/components/mesh/branch/util.tsx
+++ src/components/mesh/branch/util.tsx
@@ -1,5 +1,5 @@
 import { values } from "../../ui/setupControls"
-import { random } from "../../util/math"
+import { DegToRad, random } from "../../util/math"
 
 export const nextBranchLen = (len: number) => {
     return len * random(values.nextLenMin, values.nextLenMax)
@@ -7,4 +7,20 @@ export const nextBranchLen = (len: number) => {
 
 export const nextBranchDia = (dia: number) => {
     return dia * random(values.nextDiaMin, values.nextDiaMax)
+}
+
+export const nextRootAngle = () => {
+    const a = random(DegToRad(values.rootAngleMin), DegToRad(values.rootAngleMax))
+    if (Math.random() > 0.5) {
+        console.log(">", a)
+        return a
+    } else {
+        const b = 0- a
+        console.log("<", b)
+        return b
+    }
+}
+
+export const nextSideAngle = () => {
+    return random(DegToRad(values.sideAngleMin), DegToRad(values.sideAngleMax))
 }
