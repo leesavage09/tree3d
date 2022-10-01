ac03edbbd1169d6e56914a78d81ceabae54333d9
leesavage09 <leesavage09@gmail.com>
2022-10-01 16:29:00 +0300

    new values in controller ui

--- src/components/ui/setupControls.tsx
+++ src/components/ui/setupControls.tsx
@@ -1,8 +1,32 @@
 import React, { useEffect } from "react";
 import * as lilGui from 'lil-gui';
 
-export const values: any = {
+interface Values {
+    rootLen: number,
+    rootDia: number,
 
+    nextLenMin: number,
+    nextLenMax: number,
+    nextDiaMin: number,
+    nextDiaMax: number,
+
+    rootSurvival: number,
+    survival1: number,
+    survival2: number,
+    survival3: number,
+    survival4: number,
+
+    rootAngleMin: number,
+    rootAngleMax: number,
+    sideAngleMin: number,
+    sideAngleMax: number,
+
+    SideMultiplier: number,
+    growth: number,
+    maxDepth: number
+}
+
+export const values: Values = {
     rootLen: 3,
     rootDia: 1,
 
@@ -11,29 +35,34 @@ export const values: any = {
     nextDiaMin: 0.50,
     nextDiaMax: 0.60,
 
-    rootSurvival: 1,
+    rootSurvival: 0,
     survival1: 1,
     survival2: 1,
-    survival3: 1,
-    survival4: 1,
+    survival3: 0,
+    survival4: 0,
 
+    rootAngleMin: 0,
+    rootAngleMax: 0,
+    sideAngleMin: 45,
+    sideAngleMax: 45,
 
     SideMultiplier: 5,
     growth: 20,
     maxDepth: 3,
 }
 
-const addMinMaxCon = (group: lilGui.GUI, min: number, max: number, minString: string, maxString: string) => {
-    const conMin = group.add(values, minString, min, max).name('min').onChange((min: any) => {
-        if (min > values[maxString]) {
-            values[maxString] = min
+const addMinMaxCon = (group: lilGui.GUI, min: number, max: number, minString: string, maxString: string, name = '') => {
+    const v = values as any;
+    const conMin = group.add(v, minString, min, max).name(name + ' min').onChange((min: any) => {
+        if (min > v[maxString]) {
+            v[maxString] = min
             conMax.updateDisplay()
         }
     });
 
-    const conMax = group.add(values, maxString, min, max).name('max').onChange((max: any) => {
-        if (max < values[minString]) {
-            values[minString] = max
+    const conMax = group.add(v, maxString, min, max).name(name + ' max').onChange((max: any) => {
+        if (max < v[minString]) {
+            v[minString] = max
             conMin.updateDisplay()
         }
     });
@@ -42,7 +71,7 @@ const addMinMaxCon = (group: lilGui.GUI, min: number, max: number, minString: st
 export const SetupControls: React.FC = () => {
 
     useEffect(() => {
-        const gui = new lilGui.GUI({ title: "Tree Controls" });
+        const gui = new lilGui.GUI({ title: "Tree Controls", container: document.getElementById('lilGui') || undefined });
 
         const rootGUI = gui.addFolder('Root Branch')
         rootGUI.add(values, 'rootLen', 0.1, 10).name('Length');
@@ -62,9 +91,13 @@ export const SetupControls: React.FC = () => {
         const nextDiaGUI = nextGUI.addFolder('Diameter')
         addMinMaxCon(nextDiaGUI, 0.1, 1, 'nextDiaMin', 'nextDiaMax')
 
+        const anglesGUI = gui.addFolder('Side Angles')
+        addMinMaxCon(anglesGUI, 0, 180, 'rootAngleMin', 'rootAngleMax', "Root angle")
+        addMinMaxCon(anglesGUI, 0, 180, 'sideAngleMin', 'sideAngleMax', "Side angle")
+
         const renderGUI = gui.addFolder('Draw')
         renderGUI.add(values, 'SideMultiplier', 3, 60, 1).name('Side Multiplier');
-        renderGUI.add(values, 'growth', 0.1, 20).name('Animation Speed');
+        renderGUI.add(values, 'growth', 0.1, 40).name('Animation Speed');
 
         return () => {
             gui.destroy()
